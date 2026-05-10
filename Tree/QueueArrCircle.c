8656f2663f9c924ae18c308c2e6cade88565265a
hukehulu <hukehulu@gmail.com>
2026-05-10 21:31:02 +0800

    QueueArrCircle

---
+++ Tree/QueueArrCircle.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxSize 100
+
+typedef int Element;
+
+struct queue{
+    Element Data[MaxSize];
+    int Front;
+    int Rear;
+};
+
+typedef struct queue Queue;
+
+Queue* Init_Queue(){
+    Queue *Q=(Queue*)malloc(sizeof(Queue));
+    Q->Front=0;
+    Q->Rear=0;
+    return Q;
+}
+
+int isEmpty(Queue *Q){
+    if(Q->Front==Q->Rear){
+        return 1;
+    }else{
+        return 0;
+        
+    }
+}
+
+int isFull(Queue *Q){
+    if(Q->Front>0){
+        int set = Q->Front;
+        for(int i=Q->Front;i<Q->Rear;i++){
+            Q->Data[i-set] = Q->Data[i];
+        }
+        Q->Front = 0;
+        Q->Rear = Q->Rear-set;
+        return 0;
+    }else{
+        return 1;
+    }
+
+}
+
+
+
+int enqueue(Queue *Q,Element x){
+    if((Q->Rear+1)%MaxSize==Q->Front){
+        printf("Queue is full\n");
+        return 0;
+    }
+    Q->Data[Q->Rear] = x;
+    Q->Rear = (Q->Rear+1)%MaxSize;
+    return 1;
+}
+
+int dequeue(Queue *Q){
+    if(isEmpty(Q)){
+        return 0;
+    }
+    Element x = Q->Data[Q->Front];
+    Q->Front = (Q->Front+1)%MaxSize;
+    return x;
+}
+
+int getFront(Queue *Q,Element *x){
+    if(isEmpty(Q)){
+        return 0;
+    }
+    *x = Q->Data[Q->Front];
+    return 1;
+}
+int main(){
+    Queue *Q = Init_Queue();
+    enqueue(Q,1);
+    enqueue(Q,2);
+    enqueue(Q,3);
+    Element x;
+    getFront(Q,&x);
+    printf("%d\n",x);
+    int y = dequeue(Q);
+    getFront(Q,&y);
+    printf("%d\n",y);
+    return 0;
+}
---
+++ Tree/QueueList.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Element;
+
+struct queuenode{
+    Element Data;
+    struct queuenode *next;
+};
+
+typedef struct queuenode Queuenode;
+
+typedef struct{
+    Queuenode *Front;
+    Queuenode *Rear;
+} Queue;
+
+Queue* InitQueue(){
+    Queue *Q = (Queue*)malloc(sizeof(Queue));
+    Queuenode *node = (Queuenode*)malloc(sizeof(Queuenode));
+    node->Data = 0;
+    node->next = NULL;
+    Q->Front = node;
+    Q->Rear = node;
+    return Q;
+}
+
+int isEmpty(Queue *Q){
+    if(Q->Front==Q->Rear){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int enQueue(Queue *Q, Element e){
+    Queuenode *node = (Queuenode*)malloc(sizeof(Queuenode));
+    node->Data = e;
+    node->next = NULL;
+    Q->Rear->next = node;
+    Q->Rear = node;
+    return 1;
+}
+
+int deQueue(Queue *Q, Element *x){
+    if(isEmpty(Q)){
+        return 0;
+    }
+    Queuenode *q = Q->Front->next;
+    *x = Q->Front->next->Data;
+    Q->Front->next = q->next;
+    if(Q->Rear == q){
+        Q->Rear = Q->Front;
+    }
+    free(q);
+    return 1;
+}
+
+int getFront(Queue *Q, Element *x){
+    if(isEmpty(Q)){
+        return 0;
+    }
+    *x = Q->Front->next->Data;
+    return 1;
+}
+int main(){
+    Queue *Q = InitQueue();
+    enQueue(Q, 1);
+    enQueue(Q, 2);
+    enQueue(Q, 3);
+    Element x;
+    getFront(Q, &x);
+    printf("%d\n", x);
+    deQueue(Q, &x);
+    printf("%d\n", x);
+    getFront(Q, &x);
+    printf("%d\n", x);
+    return 0;
+}
